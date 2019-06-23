139166ce6c36882d158c6129f6b25884f559852c
Akihiro Ikezoe <a.ikezoe@gmail.com>
2019-06-23 22:07:59 +0900

    fix

--- 02_dockerfile/helloworld/hello.js
+++ 02_dockerfile/helloworld/hello.js
@@ -1,7 +1,7 @@
 // copied from https://nodejs.org/en/about/
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
